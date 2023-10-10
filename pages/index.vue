b7e1822e2d282761a614a3fcf69ce3dc4eeda4bb
John Alesi Resurreccion <resurreccionjohnalesi@gmail.com>
2023-10-10 23:07:56 +0800

    Task title

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,19 @@
 </script>
 
 <template>
-    <main class="flex flex-col items-center pt-[450px] min-h-screen container">
-        <InputTask />
+    <main
+      class="flex flex-col items-center pt-[450px] min-h-screen container"
+    >
+      <div
+        class="flex flex-col items-center gap-[.8rem] text-center mb-8"
+      >
+        <h1 class="font-medium text-3xl">
+          Welcome to Task Planner
+        </h1>
+        <h2 class="max-w-[26.8rem] font-normal text-md">
+          Get things done with your day, a list that refreshes everyday
+        </h2>
+      </div>
+      <InputTask />
     </main>
-</template>
+  </template>
